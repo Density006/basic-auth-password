fcc8d4d2de2e7b7e336dfc518e5a88b33d8532cd
Density006 <nate@studio.digital>
2025-10-23 21:22:24 +0800

    Update auth.ts

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,7 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
-  res.setHeader('WWW-authenticate', 'Basic realm="Secure Area"')
+  // By adding a unique timestamp, the "realm" is different on every request.
+  // This forces the browser to re-prompt for login.
+  const uniqueRealm = `Secure Area - ${Date.now()}`
+  
+  res.setHeader('WWW-authenticate', `Basic realm="${uniqueRealm}"`)
   res.statusCode = 401
   res.end(`Auth Required.`)
 }
