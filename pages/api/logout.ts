bdf255b5c1b7e5c549f98a54018aefdd09991e59
Density006 <nate@studio.digital>
2025-10-23 23:10:25 +0800

    Update logout.ts

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,11 +1,11 @@
 import { withIronSessionApiRoute } from 'iron-session/next'
-import { sessionOptions } from '@/lib/session'
+import { sessionOptions } from '../../lib/session' // <-- This line is fixed
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 function logoutRoute(req: NextApiRequest, res: NextApiResponse) {
   // Destroy the session
   req.session.destroy()
-
+  
   // Redirect the user to the login page
   res.setHeader('location', '/login')
   res.status(302).end()
